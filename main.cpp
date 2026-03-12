2154025b2d95d09d1f623eec0a07cba603284058
yujin010203 <yyuyu02@naver.com>
2026-03-12 09:59:30 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,14 @@
 #include <iostream>
+#include <ctime>
 
 using namespace std;
 
-// 45개 공 중에서 6개를 뽑는 프로그램
+// 6개의 공을 랜덤으로 뽑는 프로그램
 
-// int* P = new int[];
-int Numbers[45];
-int PlayerNumber[6];
 
-void Init()
+void Init(int* Numbers, int Size)
 {
-	for (int i = 0; i < 45; i++)
+	for (int i = 0; i < Size; i++)
 	{
 		Numbers[i] = i + 1;
 	}
@@ -18,12 +16,12 @@ void Init()
 	srand((unsigned int)time(nullptr));
 }
 
-void Shuffle()
+void Shuffle(int* Numbers, int Size)
 {
-	for (int i = 0; i < 45; i++)
+	for (int i = 0; i < Size*3; i++)
 	{
-		int FirstIndex = rand() % 45;
-		int SecondIndex = rand() % 45;
+		int FirstIndex = rand() % Size;
+		int SecondIndex = rand() % Size;
 		int Temp = Numbers[FirstIndex];
 
 		Numbers[FirstIndex] = Numbers[SecondIndex];
@@ -31,24 +29,33 @@ void Shuffle()
 	}
 }
 
-void Deal()
-{
-	for (int i = 0; i < 6; i++)
-	{
-		PlayerNumber[i] = Numbers[i * 2];
-	}
-}
-
 int main()
 {
-	Init();
-	Shuffle();
-	Deal();
+	int Size;
+	int* Numbers = nullptr;
+
+	cout << "공의 개수를 입력해주세요 : ";
+	cin >> Size;
+
+	Numbers = new int[Size];
+
+
+	Init(Numbers, Size);
+	Shuffle(Numbers, Size);
+
+	int PlayerNumber[6];
+
+	for (int i = 0; i < 6; i++)
+	{
+		PlayerNumber[i] = Numbers[i];
+	}
 
 	for (int i = 0; i < 6; i++)
 	{
 		cout << PlayerNumber[i] << " ";
 	}
 
+	delete[] Numbers;
+	Numbers = nullptr;
 	return 0;
 }
